84577ecdbe76903d59eaa375f7d02bfcfd3202a1
omarmung <dkimmel@gmail.com>
2016-08-06 16:41:22 -0700

    complete dots and cursor

--- client/watchout.js
+++ client/watchout.js
@@ -12,16 +12,115 @@
 // give them random starting coordinates
 // // render svg through d3
 
-var asteroids = [];
-for (var i = 0; i < 30; i++) {
-  asteroids.push({
-    name: 'ast' + i,
-    height: '40 px',
-    width: '50 px'
-  });
-}
 
-// Math.random(max -(2 * min) + 1 - min)
+// make a palyer circle
+// bind that to mouse
+// collision detection
+// 
+
+
+var player1Data = [{
+  posX: 100,
+  posY: 100,
+  height: 30,
+  width: 30,
+  fill: 'black'
+}];
+
+var updatePlayer1 = function(data) {
+  console.log('running player1');
+  var selection = d3.select('svg')
+    .selectAll('circle')
+    .data(data);
+
+  selection
+    // .selectAll('circle')
+    .transition().duration(3000)
+    .attr('width', function(d) { return d.width; })
+    .attr('height', function(d) { return d.height; })
+    .attr('fill', function(d) { return d.fill; });
+
+  selection
+    .enter()
+    .append('rect')
+    .attr('width', function(d) { return d.width; })
+    .attr('height', function(d) { return d.height; })
+    .attr('fill', function(d) { return d.fill; });
+
+  selection
+    .exit().remove();
+};
+
+var started = function () {
+  var rectangle = d3.select(this).classed("dragging", true);
+
+  d3.event.on("drag", dragged).on("end", ended);
+
+  var dragged = function (d) {
+    rectangle.raise().attr('width', d.height = d3.event.x).attr("height", d.width = d3.event.y);
+  };
+
+  var ended = function() {
+    rectangle.classed("dragging", false);
+  };
+};
+
+var makeRandomAsteroids = function(num) {
+
+  var asteroids = [];
+  for (var i = 0; i < num; i++) {
+    console.log('making new set of asteroids!');
+    asteroids.push({});
+    asteroids[i].name = 'ast' + i;
+    asteroids[i].height = (Math.floor(Math.random() * (349) + 50));
+    asteroids[i].width = (Math.floor(Math.random() * (599) + 50));
+    asteroids[i].radius = (Math.floor(Math.random() * 50))// 30;
+    asteroids[i].fill = 'red';
+  }
+  return asteroids;
+};
+
+var updateAsteroids = function(data) {
+  console.log('running create!');
+  var selection = d3.select('svg')
+    .selectAll('circle')
+    .data(data);
+
+  selection
+    // .selectAll('circle')
+    .transition().duration(3000)
+    .attr('cx', function(d) { return d.width; })
+    .attr('cy', function(d) { return d.height; })
+    .attr('r', function(d) { return d.radius; })
+    .attr('fill', function(d) { return d.fill; });
+
+  selection
+    .enter()
+    .append('circle')
+    .attr('cx', function(d) { return d.width; })
+    .attr('cy', function(d) { return d.height; })
+    .attr('r', function(d) { return d.radius; })
+    .attr('fill', function(d) { return d.fill; });
+
+  selection
+    .exit().remove();
+};
+
+var asteroidsNum = 20;
+
+updatePlayer1(player1Data);
+
+updateAsteroids(makeRandomAsteroids(asteroidsNum));
+updateAsteroids(makeRandomAsteroids(asteroidsNum));
+setInterval(function() { updateAsteroids(makeRandomAsteroids(asteroidsNum)); }, 4000);
+d3.selectAll("rect").call(d3.drag().on("start", started));
+
+
+
+
+
+
+
 
 var generateCollection = function(productName, number, attributes) {
   // create('asteroid', 5, 'svg', 'width: 50 px', 'height: 60 px', 'whatever I want: cool')
@@ -109,7 +208,9 @@ var update = function(items) {
 
 // console.log(asteroids);
 // update(asteroids);
-create(generateCollection('asteroid', 3));
+// create(generateCollection('asteroid', 3));
+
+
 // update(generateCollection('asteroid', 3, { fill: 'green' }));
 // update(generateCollection('asteroid', 10, { fill: 'green' }));
 // update(generateCollection('asteroid', 10, { fill: 'green' }));
@@ -120,3 +221,7 @@ create(generateCollection('asteroid', 3));
 // testMake();
 // testMake();
 // testUpdate();
+
+
+// Welcome to the library! Shhh!
+// Math.random(max -(2 * min) + 1 - min)
